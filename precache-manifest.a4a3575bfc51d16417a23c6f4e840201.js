a2a607026ee6bedb975a6feb1c24c07c79113ec4
yuulye <yuulye@gmail.com>
2019-07-20 13:44:21 +0700

    Updates

---
+++ precache-manifest.a4a3575bfc51d16417a23c6f4e840201.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad463a50487c4e5d3d5863a6ba7a93e2",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "6ce7d1360951bbeaef30",
+    "url": "/tictactoe/static/css/main.81302117.chunk.css"
+  },
+  {
+    "revision": "283cbb3238da73526ab7",
+    "url": "/tictactoe/static/js/2.21e67013.chunk.js"
+  },
+  {
+    "revision": "6ce7d1360951bbeaef30",
+    "url": "/tictactoe/static/js/main.fa16ae3b.chunk.js"
+  },
+  {
+    "revision": "e9d42febdd2440e2e0cd",
+    "url": "/tictactoe/static/js/runtime~main.1cfd47c4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tictactoe/static/media/logo.5d5d9eef.svg"
+  }
+]);
